3bd8c23c18a62acec93526215a374d4fa10153d8
chuoru <tranvietthanh0101@gmail.com>
2020-02-14 19:40:48 +0900

    Update

--- src/server/engine/main.py
+++ src/server/engine/main.py
@@ -1,1 +1,17 @@
 #coding: utf-8
+
+import numpy as np 
+
+class new_class: 
+    def __init__(self, value): 
+        self.value = value 
+
+    def new_function(self):
+        return self.value * 2 
+
+if __name__ = "__main__":
+    value = 10 
+
+    first_class = new_class(value)
+    print(first_class.new_function)
+
